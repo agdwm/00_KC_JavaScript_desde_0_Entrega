9a4f6178e9a66b596177562d9171b321a256f677
agdwm <almudenaguerraswm@gmail.com>
2018-08-02 12:13:41 +0200

    Created the grid for the first level

---
+++ display.js
@@ -0,0 +1,22 @@
+
+const SCALE = 20;
+
+function createElement(type, className) {
+	let element = document.createElement(type);
+
+	if(className !== undefined) {
+		element.className = className;
+		return element;
+	}
+}
+
+function DOMDisplay(parent, level) {
+	this.wrap = parent.appendChild(createElement('div', 'game'));
+	this.level = level;
+
+	// this.wrap.appendChild();
+	this.drawBackground = function() {
+		let table = createElement('table', 'background');
+		table.style.width = this.level.width * SCALE + 'px';
+	}
+}
