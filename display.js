b1530e56dafa51c3bb5097faaa07f486e840b709
agdwm <almudenaguerraswm@gmail.com>
2018-08-02 13:03:55 +0200

    Draw the grid of the game into the html

--- display.js
+++ display.js
@@ -1,10 +1,9 @@
-
 const SCALE = 20;
 
 function createElement(type, className) {
 	let element = document.createElement(type);
 
-	if(className !== undefined) {
+	if (className) {
 		element.className = className;
 		return element;
 	}
@@ -13,10 +12,19 @@ function createElement(type, className) {
 function DOMDisplay(parent, level) {
 	this.wrap = parent.appendChild(createElement('div', 'game'));
 	this.level = level;
-
-	// this.wrap.appendChild();
+	
 	this.drawBackground = function() {
 		let table = createElement('table', 'background');
 		table.style.width = this.level.width * SCALE + 'px';
+
+		this.level.grid.forEach(row => {
+			let rowElement = createElement('tr');
+			rowElement.style.height = SCALE + 'px';
+			table.appendChild(rowElement);
+			row.forEach(type => rowElement.appendChild(createElement('td', type)));
+		});
+		return table;
 	}
+
+	this.wrap.appendChild(this.drawBackground());
 }
